7533f93aa63e350d0f511cb053e77ab4d7ee4cfc
coreyb03 <thecorey03@gmail.com>
2019-12-02 08:34:13 -0700

    initial

---
+++ thxgiving.js
@@ -0,0 +1,17 @@
+document.getElementById("title").style.fontSize = "75px";
+document.getElementById("title").style.color = "White";
+document.getElementById("title").style.fontFamily = "helvetica";
+document.getElementById("title").style.textAlign = "center";
+document.body.style.backgroundImage = "url('turkeygif2.gif')";
+document.body.style.backgroundSize= "12.5%";
+function playAudio(){
+    var gobble = document.getElementById("gobble");
+    gobble.play();
+}
+document.getElementById("turkey").style.display = "block";
+document.getElementById("turkey").style.margin = "0 auto";
+
+
+
+
+
